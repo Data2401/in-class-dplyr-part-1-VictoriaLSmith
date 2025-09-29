b22960d3eea0ad7376fabebc4155a72bc1b2d5dd
Lindsey Smith <smithl140@gator.uhd.edu>
2025-09-29 11:13:00 -0500

    Finished practice

--- DplyrDay1Practice.R
+++ DplyrDay1Practice.R
@@ -23,6 +23,13 @@ help(starwars)
 # Create a new variable: bmi = mass / (height/100)^2
 # ARRANGE
 # Sort by bmi, highest first.
+starwars %>%
+  select(name,height,mass,species,homeworld) %>%
+  filter(height > 180) %>%
+  mutate('bmi'=mass/(height/100)^2) %>%
+  arrange(desc(bmi))
+
+
 
 
 
@@ -40,15 +47,26 @@ help(starwars)
 # ARRANGE:
 # Sort by mass, lowest first.
 
-
-
-
+starwars %>%
+  mutate(height_m = height/100) %>%
+  arrange(mass) %>%
+  select(name,homeworld,species) %>%
+  filter(species == 'Human')
 
 # --------------------------------------------------
 # Reflection Questions:
 # --------------------------------------------------
 # Disclaimer: I haven't run these yet :) 
 # a) Which character has the highest BMI among those taller than 180 cm?
+print('IG-88 has the the highest BMI')
 # b) Who is the tallest human character in the dataset?
+starwars %>%
+  filter(species=='Human') %>%
+  arrange(desc(height)) %>%
+  select(name,height)
+print("Darth Vader is the tallest human character")
 # c) Which homeworld has the most characters taller than 180 cm?
+starwars %>%
+  filter(height>180) %>%
+
 # d) Does the character with the highest BMI surprise you? Why or why not?
